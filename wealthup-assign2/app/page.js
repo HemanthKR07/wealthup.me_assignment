df331e1c7ef6dffd3fc11b516c970583d9d637cb
Hemanth KR <krh7799@gmail.com>
2024-01-10 14:40:21 +0530

    Working on UI

--- wealthup-assign2/app/page.js
+++ wealthup-assign2/app/page.js
@@ -1,15 +1,20 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+// import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-       
-
-      <div className={styles.center} style={{marginLeft:"450px", marginTop:"60px"}}>
-      </div>
-      </div>
-    </main>
+    <>
+    <style>
+  @import url('https://fonts.googleapis.com/css2?family=Alegreya:ital@1&family=Amatic+SC:wght@700&family=Architects+Daughter&family=Belanosima&family=Bree+Serif&family=Caveat:wght@700&family=Chakra+Petch:wght@500&family=Courier+Prime&family=Dosis&family=Edu+TAS+Beginner:wght@700&family=Exo+2:wght@300&family=Fasthand&family=Fredericka+the+Great&family=Handjet:wght@300&family=Indie+Flower&family=Inter:wght@300&family=Kalam&family=Lugrasimo&family=Lumanosimo&family=Merienda:wght@500&family=Montserrat+Alternates&family=Montserrat:wght@200;300;400&family=Noto+Sans+Indic+Siyaq+Numbers&family=Nunito:wght@200&family=Pangolin&family=Poppins:wght@100&family=Rajdhani&family=Raleway:wght@300&family=Red+Hat+Display:wght@500&family=Rubik&family=Rubik+Scribble&family=Sacramento&family=Sarabun&family=Sedgwick+Ave+Display&family=Single+Day&family=Slabo+27px&family=Tajawal&family=Titillium+Web:wght@300&family=Ysabeau+Infant:wght@200&family=Ysabeau+SC:wght@300&family=Zeyada&display=swap');
+</style>
+    <div className="block">
+          <h2 className="t1">Assignment 2</h2>
+          <h5 className="t2">Verify by entering the code.</h5>
+          <br />
+          <h5 className="t3">Code :</h5>
+          <p className="code">qwR67y</p>
+          <input type="text" name="input" id="i1" maxLength={5} placeholder="Type here"/>
+          {/* <hr className="hr1" /> */}
+    </div>
+    </>
   )
 }
